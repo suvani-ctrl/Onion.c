b9e0bfdea64fc0d02e7fcc97bda0cb57a26ca2a7
suvani-ctrl <sssubbbi1234@gmail.com>
2024-11-20 21:55:36 +0545

    Create O.c
    First Push

---
+++ O.c
@@ -0,0 +1,19 @@
+#include "toralize.h"
+
+int main(int argc, char *argc[])
+{
+    char *host;
+    int port;
+
+    int (argc < 3)
+    {
+        frintf(stderr, "Usage: %s <host> <port> \n", argv[0]);
+        return -1;
+    }
+}
+/*
+./toralize 1.2.3.4 80  
+argv[0] , argv[1], argv[2]
+*/
+Host = argv[1];
+Port = atoi(argv[2]);
