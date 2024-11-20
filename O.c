fe3b5534d3684fb6e4481b7a0e1fd87705b08333
suvani-ctrl <sssubbbi1234@gmail.com>
2024-11-20 22:28:13 +0545

    Update O.c
    Typo and Code edited <3

--- O.c
+++ O.c
@@ -1,19 +1,46 @@
-#include "toralize.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
 
-int main(int argc, char *argc[])
+int main(int argc, char *argv[])
 {
     char *host;
     int port;
 
-    int (argc < 3)
+    if (argc < 3)
     {
-        frintf(stderr, "Usage: %s <host> <port> \n", argv[0]);
+        fprintf(stderr, "Usage: %s <host> <port>\n", argv[0]);
         return -1;
     }
+
+    host = argv[1];
+    port = atoi(argv[2]);
+
+    typedef unsigned char int8;
+    typedef unsigned short int int16;
+    typedef unsigned int int32;
+
+    struct Proxy_Request
+    {
+        int8 vn;
+        int8 CD;
+        int16 Destination_Port;
+        int32 Destination_IP;
+        unsigned char userid[8];
+    };
+
+    struct Proxy_Response
+    {
+        int8 vn;
+        int8 CD;
+        int16 _;
+        int32 __;
+    };
+
+    typedef struct Proxy_Request Req;
+    typedef struct Proxy_Response Resp;
 }
-/*
-./toralize 1.2.3.4 80  
-argv[0] , argv[1], argv[2]
-*/
-Host = argv[1];
-Port = atoi(argv[2]);
