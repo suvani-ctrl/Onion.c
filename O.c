02cf34c7be9b004fed80d97ef7829ddb970422c2
suvani-ctrl <sssubbbi1234@gmail.com>
2024-11-21 22:39:36 +0545

    Update O.c
    UPDATED 2X

--- O.c
+++ O.c
@@ -6,50 +6,61 @@
 #include <arpa/inet.h>
 #include <netinet/in.h>
 
-int main(int argc, char *argv[])
-{
-    char *host;
-    int port;
+#define PROXY "127.0.0.1"
+#define PROXYPORT 1234
+#define USERNAME "onion"
+#define reqsize sizeof(struct Proxy_Request)
+#define ressize sizeof(struct Proxy_Response)
 
-    if (argc < 3)
-    {
+typedef unsigned char int8;
+typedef unsigned short int int16;
+typedef unsigned int int32;
+
+struct Proxy_Request {
+    int8 vn;
+    int8 CD;
+    int16 Destination_Port;
+    int32 Destination_IP;
+    unsigned char userid[8]; 
+};
+
+struct Proxy_Response {
+    int8 vn;
+    int8 CD;
+    int16 _;
+    int32 __; 
+};
+
+struct Proxy_Request *request(const char *dstip, const int dstport) {
+    struct Proxy_Request *req = malloc(reqsize);
+    if (req == NULL) {
+        perror("Failed to allocate memory");
+        return NULL;
+    }
+
+    req->vn = 4;
+    req->CD = 1;
+    req->Destination_Port = htons(dstport);
+    req->Destination_IP = inet_addr(dstip);
+    strncpy((char *)req->userid, USERNAME, 8);
+
+    return req;
+}
+
+int main(int argc, char *argv[]) {
+    if (argc < 3) {
         fprintf(stderr, "Usage: %s <host> <port>\n", argv[0]);
         return -1;
     }
 
-    host = argv[1];
-    port = atoi(argv[2]);
-
-    typedef unsigned char int8;
-    typedef unsigned short int int16;
-    typedef unsigned int int32;
-
-    struct Proxy_Request
-    {
-        int8 vn;
-        int8 CD;
-        int16 Destination_Port;
-        int32 Destination_IP;
-        unsigned char userid[8];
-    };
-
-    struct Proxy_Response
-    {
-        int8 vn;
-        int8 CD;
-        int16 _;  
-        int32 __; 
-    };
-
-    typedef struct Proxy_Request Req;
-    typedef struct Proxy_Response Resp;
+    char *host = argv[1];
+    int port = atoi(argv[2]);
 
     int SocketFd;
     struct sockaddr_in sock;
 
     SocketFd = socket(AF_INET, SOCK_STREAM, 0);
-    if (SocketFd < 0)
-    {
+    if (SocketFd < 0) {
         perror("Socket Error!");
         return -1;
     }
@@ -58,26 +69,28 @@ int main(int argc, char *argv[])
     sock.sin_port = htons(port);
     sock.sin_addr.s_addr = inet_addr(host);
 
-    if (connect(SocketFd, (struct sockaddr *)&sock, sizeof(sock)) < 0)
-    {
-        perror("connect");
+    if (connect(SocketFd, (struct sockaddr *)&sock, sizeof(sock)) < 0) {
+        perror("Connect Error");
         return -1;
     }
-    printf("Connected To The Proxy\n");
-    close(SocketFd);
 
-//chech here pls 
-    Req *request(const char *dstip, const int dstport)
-    {
-        Req *req;
-        req = malloc(reqsize);
-        req -> vn = 4; // -> is basically same as . format but when were using pointe rto a struct we use it !
-        req -> cd = 1;
-        req -> Destination_Port = htons(1);
-        req -> Destination_IP = inet_addr(Destination_IP);
-
+    printf("Connected to the Proxy\n");
+    struct Proxy_Request *req = request(PROXY, PROXYPORT);
+    if (req != NULL) {
+        write(SocketFd, req, reqsize);
+        free(req);
     }
-//here 
+
+    char buf[ressize];
+    memset(buf, 0, ressize);
+
+    if (read(SocketFd, buf, ressize) < 1) {
+        perror("read");
+        close(SocketFd);
+        return -1;
+    }
+
+    close(SocketFd);
 
     return 0;
 }
